00f4ce32c13c2139edbe9fc784afc44129972bf5
Sayak Mondal <sayakmondal2001@gmail.com>
2022-11-26 23:50:51 +0530

    Final commit

---
+++ app/src/main/java/com/example/internassignment/success.java
@@ -0,0 +1,17 @@
+package com.example.internassignment;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.success);
+
+
+    }
+}
